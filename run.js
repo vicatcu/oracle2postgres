c882f2c4f3199c926e73d89df21d642795d19150
Victor Aprea <victor.aprea@wickeddevice.com>
2023-07-20 12:18:21 -0400

    fix

---
+++ run.js
@@ -0,0 +1,26 @@
+const { readFile, readdir } = require('fs/promises');
+const path = require('path');
+async function run() {
+  const filesToTranslate = await readdir(path.resolve('./', 'oracle-queries'));
+  for (const filename of filesToTranslate) {
+    process.stdout.write(`Translating "${filename}"...`);
+    try {
+      let content = await readFile(path.resolve('./', 'oracle-queries', filename), 'utf8');
+      content = content.toLowerCase();
+      content = convertSubstringFunctions(content);
+
+      process.stdout.write('Complete.\r\n');
+    } catch (e) {
+      process.stdout.write('Failed.\r\n');
+      console.error(e);
+    }
+  }
+}
+function convertSubstringFunctions(content) {
+  // look for a substring function
+  // if you find one, convert it
+  // repeat until you don't find one
+
+  return content;
+}
+run().then(() => console.log('Done.')).catch(e => console.error(e));
