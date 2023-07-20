c0b3dd344d24e06ef77030f4f1631ff497a8c182
Victor Aprea <victor.aprea@wickeddevice.com>
2023-07-20 13:11:27 -0400

    change-folder name

--- run.js
+++ run.js
@@ -1,4 +1,4 @@
-const { readFile, readdir } = require('fs/promises');
+const { readFile, readdir, writeFile } = require('fs/promises');
 const path = require('path');
 async function run() {
   const filesToTranslate = await readdir(path.resolve('./', 'oracle-queries'));
@@ -10,8 +10,9 @@ async function run() {
     process.stdout.write(`Translating "${filename}"...`);
     try {
       let content = await readFile(path.resolve('./', 'oracle-queries', filename), 'utf8');
-      content = content.toLowerCase();
       content = convertSubstringFunctions(content);
+
+      await writeFile(path.resolve('./', 'postgres-queries', filename), content, 'utf8');
 
       process.stdout.write('Complete.\r\n');
     } catch (e) {
@@ -21,10 +22,50 @@ async function run() {
   }
 }
 function convertSubstringFunctions(content) {
-  // look for a substring function
-  // if you find one, convert it
-  // repeat until you don't find one
+  const ret = [];
+  let fromEncountered = false;
+  let lines = content.split(/[\n\r]/);
+  const repMap = [];
+  for (const line of lines) {
+    let newLine = line;
+    if (line.toLowerCase().includes('from')) {
+      fromEncountered = true;
+    }
 
-  return content;
+    if (line.toLowerCase().includes('dbms_lob')) {
+      // handle dbms_lob
+
+    } else {
+      let re = /\.([a-z0-9_]+)[, ]/i;
+      let match = re.exec(line);
+      if (match?.[1]) {
+        const oldString = '.' + match[1]
+        const newString = oldString.toLowerCase();
+        repMap.push({oldString, newString, match: match[1]});
+      }
+
+      re = /([a-z0-9_]+)\./i;
+      match = re.exec(line);
+      if (match?.[1]) {
+        const oldString = match[1] + '.';
+        const newString = oldString.toLowerCase();
+        repMap.push({oldString, newString, match: match[1]});
+      }
+
+
+      for (const rep of repMap) {
+        const oString = rep.oldString;
+        const nString = rep.newString;
+        const match = rep.match;
+        newLine = newLine.replace(oString, nString);
+        if (!newLine.toLowerCase().includes(nString + ' as ') && !fromEncountered) {
+          // if there's not an 'AS' then add one
+          newLine = newLine.replace(nString , `${nString} AS "${match}"`);
+        }
+      }
+    }
+  }
+
+  return ret.join('\n');
 }
 run().then(() => console.log('Done.')).catch(e => console.error(e));
